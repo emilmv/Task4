4a5d757d5a73e72999bd0b33224b5451d0c8731f
emilmv <emilnm@code.edu.az>
2024-02-13 01:51:47 +0400

    Tasks

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Hello, World!");
+#region Task1
+//int n = 10;
+//if (n == 0)
+//{
+//    Console.WriteLine("Duzgun eded daxil edin");
+//}
+//else
+//{
+//    if (n % 2 == 1)
+//    {
+//        Console.WriteLine("Tekdir");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Cutdur");
+//    }
+//}
+#endregion
+#region Task2
+//int n = 10;
+//if (n % 21 == 0)
+//{
+//    Console.WriteLine("həm 3, həm 7-ə bölünür");
+//}
+//else
+//{
+//    Console.WriteLine("Bölünmür");
+//}
+#endregion
+#region Task3
+//int n = 10;
+//int m = 12;
+//if ((n + m) % 2 == 0)
+//{
+//    Console.WriteLine(n+m);
+//}
+//else
+//{
+//    Console.WriteLine("Ededlerden hec olmasa biri cut deyil");
+//}
+#endregion
+#region Task4
+//int n = 10;
+//if (n >= 15)
+//{
+//    Console.WriteLine("Hava istidir");
+//}
+//else
+//{
+//    Console.WriteLine("Hava soyuqdur");
+//}
+#endregion
